e807ad01cdb30ee173d456c5919e1da6845bef08
Emidio Valeretto <53981181+emidiovaleretto@users.noreply.github.com>
2024-12-31 21:19:58 +0000

    chore: configure production database

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -15,20 +15,19 @@ from pathlib import Path
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY_DEVELOPMENT")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "newrealestate-dced12b5caf5.herokuapp.com",
     "localhost",
 ]
 
